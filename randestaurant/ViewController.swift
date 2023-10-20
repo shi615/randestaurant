460e3d47d69eb4de3cf031e2ae62df87d4ebc172
Tomomitsu Ishi <tomomitsu.ishi@i-zuna.co.jp>
2023-10-20 15:09:10 +0900

    created a new project

---
+++ randestaurant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  randestaurant
+//
+//  Created by 石智光 on 2023/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
